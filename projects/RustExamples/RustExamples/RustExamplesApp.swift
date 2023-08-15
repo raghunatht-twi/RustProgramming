05a92e95d0be5da785b72e0a224bad1a9836d317
Raghunath T <traghunath98@gmail.com>
2023-08-15 22:43:48 +0530

    Added more examples from the book

---
+++ projects/RustExamples/RustExamples/RustExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustExamplesApp.swift
+//  RustExamples
+//
+//  Created by Tripasuri Raghunath on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RustExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
