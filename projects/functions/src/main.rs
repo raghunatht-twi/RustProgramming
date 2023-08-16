2553423dbba989c920eac994da14daa704798bba
Raghunath T <raghunath.t@thoughtworks.com>
2023-08-17 05:02:55 +0530

    Committing latest updates on functions

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -16,6 +16,14 @@ fn main() {
     }; 
     another_function(x);
 
+    //call function with return value
+    let x_fn_return = five_hundred();
+    println!("The value of x_fn_return is: {x_fn_return}");
+
+    //call plus one function
+    let x_plus_one = plus_one(762);
+    println!("The value of x_plus_one is:{x_plus_one}");
+
 }
 
 //define another_function below
@@ -32,4 +40,16 @@ fn print_labeled_measurement(value: i32, unit_label: char){
     
     println!("The measurement is: {value}{unit_label}");
 
+}
+
+//define function with return value
+fn five_hundred() -> i32 {
+    500
+}
+
+//define funciton plus_one
+fn plus_one (x: i32) -> i32{
+   
+    //do not use semicolon here as it needs to  evaluate to an expression
+    x+1
 }
