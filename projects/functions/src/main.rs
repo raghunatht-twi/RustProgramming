2fe2d8fe973cec502c87ef46dc4be30fc5c37fd4
Raghunath T <raghunath.t@thoughtworks.com>
2023-08-16 21:41:53 +0530

    Added a few changes

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -4,6 +4,16 @@ fn main() {
 
     //call another function
     another_function(257489);
+    
+    //call print labeled measurment function
+    print_labeled_measurement(46,'h');
+
+    //statements and expressions
+    let x = {
+        let y = 2;
+        y + 4
+    }; 
+    another_function(x);
 
 }
 
@@ -13,5 +23,12 @@ fn another_function( x: i32){
 
     println!("I am in another function (as part of functions)");
     println!("The value of x: is {x}");
+
+}
+
+//define labeled measurements function
+fn print_labeled_measurement(value: i32, unit_label: char){
+    
+    println!("The measurement is: {value}{unit_label}");
 
 }
