71d87eb6e75be5f0833f7cc4f58f62f33f49fb93
Raghunath T <raghunath.t@thoughtworks.com>
2023-08-19 18:36:44 +0530

    Experiment with functions and ownership transfer

--- projects/ls_ownership/src/main.rs
+++ projects/ls_ownership/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
 
     println!("The value of x is {x} and y is {y}");
 
+    makes_copy(x); //makes copy of the integer
+
+    println!("The value of x^2 is {}", x * x);
+
     //variable s is not in scope here, as it's not yet defined
     {
         let s = "hello"; //variable s is initiated here and it gets in scope
@@ -25,6 +29,20 @@ fn main() {
     let s1 = String::from("Deep mind");
     println!("For the first time: {s1}");
 
+    takes_ownership(s1.clone()); //s1's value moves to the function
+
     let s2 = s1.clone();
     println!("For the second time S1: {s1} and S2: {s2}");
+}
+
+//Function takes ownership implementation
+fn takes_ownership(my_string: String) {
+    //my_string comes into scope
+    println!("My string value is: {my_string}");
+}
+
+//Function to make copy
+fn makes_copy(my_integer: i32) {
+    //my_integer comes into scope here
+    println!("My integer value is: {my_integer}");
 }
